a3a419775950e7c8af1c5119e9f7cccdc2b19a11
Ihor <poshonchik@gmail.com>
2023-11-15 10:59:20 +0200

    Add black SwiftUI project with CoreData enabled

---
+++ JuniorTest/JuniorTest/JuniorTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  JuniorTestApp.swift
+//  JuniorTest
+//
+//  Created by Ihor on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct JuniorTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
